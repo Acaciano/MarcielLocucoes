281a87611f63d54c00dcf8219560be0d2b8f8da1
Acaciano Neves Souza <Acaciano Neves Souza>
2017-02-06 07:55:19 -0200

    Alterando as imagens

--- AA.MarcielLocucoes/AA.MarcielLocucoes.Presentation.App/obj/Release/Package/PackageTmp/app/controllers/contato-controller.js
+++ AA.MarcielLocucoes/AA.MarcielLocucoes.Presentation.App/obj/Release/Package/PackageTmp/app/controllers/contato-controller.js
@@ -11,52 +11,13 @@ angular.module('app').controller('ContatoController', function ($scope, $http, g
 
             $scope.loading = true;
 
-            //$http({
-            //    method: 'POST',
-            //    url: global.baseUrl + '/v1/contact/send',
-            //    data: $scope.contactModel
-            //}).then(function successCallback(response) {
-            //    if (response.data.success) {
-            //        $scope.message = response.data.message;
-            //        $scope.boxMessageSuccess = true;
-            //        $scope.boxMessageError = false;
-            //        $scope.loading = false;
-
-            //        setTimeout(function () {
-            //            location.href = '/';
-            //        }, 4000);
-
-            //    } else {
-            //        $scope.message = response.data.message;
-            //        $scope.boxMessageSuccess = false;
-            //        $scope.boxMessageError = true;
-            //        $scope.loading = false;
-            //    }
-            //}, function errorCallback(response) {
-            //    $scope.message = "Não foi possivel enviar o contato, tente novamente mais tarde.";
-
-            //    $scope.boxMessageSuccess = false;
-            //    $scope.boxMessageError = true;
-            //    $scope.loading = false;
-            //});
-
-
-            //função que será usada para preparar os dados no formato que poderemos usar no servidor para envio do email
-			var param = function(data) {
-				var returnString = '';
-				for (d in data){
-					if (data.hasOwnProperty(d))
-					   returnString += d + '=' + data[d] + '&';
-				}
-				// Remove o último & que não é necessário
-				return returnString.slice( 0, returnString.length - 1 );
-			};
+            var value = $.param($scope.contactModel);
 
             $http({
                 method: 'POST',
                 url: 'email.php',
-				data : param($scope.contactModel),
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                data: value,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
             })
 		    .success(function (data) {
 		        if (data.enviado) {
